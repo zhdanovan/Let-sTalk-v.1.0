725a9596bc4663be7e63a7bf81da9f4f905bea27
Artem <artemcka.jdanov@yandex.ru>
2020-11-04 19:34:34 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-     import React from 'react';
-     import ReactDOM from 'react-dom';
-     import App from './App';
-     import { BrowserRouter as Router } from 'react-router-dom';
-     import { createBrowserHistory } from 'history
-     
-  const history = createBrowserHistory();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createBrowserHistory} from 'history'
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
     <Router history={history}>
