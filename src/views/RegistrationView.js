018362faa5a580376037b6cfef051cd4f541638a
Artem <artemcka.jdanov@yandex.ru>
2020-11-04 19:34:56 +0700

    Update RegistrationView.js

--- src/views/RegistrationView.js
+++ src/views/RegistrationView.js
@@ -14,21 +14,21 @@ export default class RegistrationView extends React.Component {
     }
 
     handleSubmit(e) {
-        const { nickname, password } = this.state;
+        const {nickname, password} = this.state;
         e.preventDefault();
         this.setState({
             successMessage: "",
             errorMessage: ""
         });
         apiServices.user
-            .create({ nickname, password })
+            .create({nickname, password})
             .then(() => this.setState({successMessage: "Success! "}))
-            .then(() => setTimeout(() => this.props.history.push('/login'),2000))
-            .catch(error => this.setState({ errorMessage: "Error! " + error.response.data.error}));
+            .then(() => setTimeout(() => this.props.history.push('/login'), 2000))
+            .catch(error => this.setState({errorMessage: "Error! " + error.response.data.error}));
     }
 
     render() {
-        const { nickname, password, successMessage, errorMessage } = this.state;
+        const {nickname, password, successMessage, errorMessage} = this.state;
         return (
             <>
                 <h1>Registration</h1>
@@ -44,7 +44,7 @@ export default class RegistrationView extends React.Component {
                                 type="text"
                                 className="nick-pass"
                                 value={nickname}
-                                onChange={e => this.setState({ nickname: e.target.value})}
+                                onChange={e => this.setState({nickname: e.target.value})}
                             />
                         </label>
                     </div>
@@ -55,7 +55,7 @@ export default class RegistrationView extends React.Component {
                                 type="password"
                                 className="nick-pass"
                                 value={password}
-                                onChange={e => this.setState({ password: e.target.value})}
+                                onChange={e => this.setState({password: e.target.value})}
                             />
                         </label>
                     </div>
