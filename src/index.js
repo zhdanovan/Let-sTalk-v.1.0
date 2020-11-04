2bc62e598f1822ef61fa4f2fd7943c7b93c3bcba
Artem <artemcka.jdanov@yandex.ru>
2020-11-04 14:09:31 +0700

    Добавлены новые import-ы

--- src/index.js
+++ src/index.js
@@ -1,80 +1,16 @@
      import React from 'react';
      import ReactDOM from 'react-dom';
      import App from './App';
+     import { BrowserRouter as Router } from 'react-router-dom';
+     import { createBrowserHistory } from 'history
      
-        ReactDOM.render(
-            <App/>,
-            document.getElementById('root')
-    );
+  const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history}>
+        <App/>
+    </Router>,
+    document.getElementById('container')
+);
 
 
-
-
-
-
-
- /*   const button = document.getElementById('button');
-    const nick = document.getElementById('nick');
-    const message = document.getElementById('message');
-    const results = document.getElementById('results');
-    button.addEventListener('click', function() {
-        //const value = input.value;
-        let xhr = new XMLHttpRequest();
-
-// 2. Настраиваем его: GET-запрос по URL
-// 3. Отсылаем запрос
-        xhr.open('POST', 'http://localhost:3000');
-// 4. Этот код сработает после того, как мы получим ответ сервера
-        xhr.onload = function() {
-            if (xhr.status != 200) {
-                console.error('Ошибка!');
-            } else {
-                const serverMessages = JSON.parse(xhr.response);
-                messages.innerHTML = '';
-                for (let serverMessage of serverMessages) {
-                    messages.innerHTML += `<ul><b>${serverMessage.nick}:</b> ${serverMessage.message}</ul>`;
-                }
-            }
-        };
-
-        xhr.onprogress = function(event) {
-            if (event.lengthComputable) {
-                console.log(`Получено ${event.loaded} из ${event.total} байт`);
-            } else {
-                console.log(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
-            }
-
-        };
-
-        xhr.onerror = function() {
-            results.innerText = "Запрос не удался";
-        };
-        xhr.send(JSON.stringify(
-            {
-                nick: nick.value ,
-                message: message.value
-            }))
-    });
-    setInterval(function() {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://localhost:3000');
-        xhr.send();
-        xhr.onload = function() {
-            if (xhr.status != 200) {
-                console.error('Ошибка!');
-            } else {
-                const serverMessages = JSON.parse(xhr.response);
-                messages.innerHTML = '';
-                for (let serverMessage of serverMessages) {
-                    messages.innerHTML += `<ul><b>${serverMessage.nick}:</b> ${serverMessage.message}</ul>`;
-                }
-            }
-        };
-    }, 1000);
-
-
-
-
-});
-
-*/
