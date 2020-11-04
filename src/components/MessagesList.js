ea1f2a67ea863d413c0d9c48dda2efedf71f0200
Artem <artemcka.jdanov@yandex.ru>
2020-11-04 19:34:22 +0700

    Update MessagesList.js

--- src/components/MessagesList.js
+++ src/components/MessagesList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Message from './Message';
+import PropTypes from "prop-types";
 
 class MessagesList extends React.Component {
     render() {
-        const { messages } = this.props;
+        const {messages} = this.props;
 
         return <>
-            { messages.map((message, index) => (
+            {messages.map((message, index) => (
                 <Message user={message.user} message={message.message} key={index}/>
-                ))}
+            ))}
         </>;
     }
 }
+
+MessagesList.propTypes = {
+    messages: PropTypes.array
+}
+
 
 export default MessagesList;
