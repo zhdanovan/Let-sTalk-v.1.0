9f8b7582fc599c4993bc7ca8b7f77a162b1040ad
Artem <artemcka.jdanov@yandex.ru>
2020-11-21 00:05:08 +0700

    Create MessageForm.js

---
+++ src/components/MessageForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class MessageForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: ''
+        };
+    }
+
+    handleSend(event) {
+        event.preventDefault();
+        this.props.postMessage({
+            content: this.state.content
+        });
+        this.setState({ content: '' });
+    }
+
+    render() {
+        const { content } = this.state;
+
+        return (
+            <form onSubmit={event => this.handleSend(event)}>
+                <input
+                    value={content}
+                    type="text"
+                    onChange={e => this.setState({ content: e.target.value })}
+                />
+                <br />
+                <button type="submit">отправить</button>
+            </form>
+        );
+    }
+}
+
+export default MessageForm;
