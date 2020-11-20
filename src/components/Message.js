23d4a374686c00f73992dd6ba916326259d94878
Artem <artemcka.jdanov@yandex.ru>
2020-11-21 00:04:57 +0700

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import PropTypes from "prop-types";
-
 
 class Message extends React.Component {
     render() {
-        const {nick, message} = this.props;
-        return <li>
-            <b>{nick}:</b>
-            {message}
-        </li>;
+        const { nickname, content } = this.props;
+        return (
+            <li>
+                <b>{nickname}:</b>
+                {content}
+            </li>
+        );
     }
 }
-
-
-Message.propTypes = {
-    nick: PropTypes.string,
-    message: PropTypes.string
-}
-
 
 export default Message;
