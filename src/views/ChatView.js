aaf3c579df62447a05689d738fc6a3c2697b4e24
Artem <artemcka.jdanov@yandex.ru>
2020-11-21 00:06:40 +0700

    Update ChatView.js

--- src/views/ChatView.js
+++ src/views/ChatView.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import axios from 'axios';
-import Form from '../components/Form';
-import MessagesList from "../components/MessagesList";
-
-
-const URL = 'http://localhost:3000';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3000'
-});
+import MessageForm from '../components/MessageForm';
+import MessagesList from '../components/MessagesList';
+import apiService from '../apiService';
 
 class ChatView extends React.Component {
     constructor() {
         super();
+
         this.state = {
-            serverMessages: []
+            messages: [],
+            users: []
         };
 
         this.timer = null;
     }
 
     componentDidMount() {
+        this.setState({ users: [], messages: [] });
         this.timer = setInterval(this.getMessages.bind(this), 1000);
     }
 
@@ -28,44 +24,50 @@ class ChatView extends React.Component {
         clearInterval(this.timer);
     }
 
-    sendMessage(newMessage) {
-        instance
-            .post('/', {
-                nick: newMessage.nick,
-                message: newMessage.message
-            })
-            .then(response => this.parseMessages(response.data))
-            .catch(error => console.error(error));
+    postMessage({ content }) {
+        apiService.message
+            .create({ content, chatId: this.props.match.params.id })
+            .then(() => this.getMessages());
     }
 
     getMessages() {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', URL);
-        xhr.send();
-        xhr.onload = () => {
-            if (xhr.status !== 200) {
-                console.error('Ошибка!');
-            } else {
-                this.parseMessages(xhr.response);
-            }
-        };
+        apiService.message
+            .getMessages(this.props.match.params.id)
+            .then(response => response.data)
+            .then(messages => this.setState({ messages }))
+            .then(() => this.getUsers())
+            .then(() => {
+                const newMessages = this.state.messages.map(message => {
+                    const user = this.state.users.find(user => user.id === message.userId);
+                    message.nickname = user.nickname;
+                    return message;
+                });
+                this.setState({ messages: newMessages });
+            });
     }
 
-    parseMessages(response) {
-        const newServerMessages = JSON.parse(response);
-        this.setState({
-            serverMessages: newServerMessages
-        });
+    getUsers() {
+        const oldUsers = this.state.users;
+        const oldUsersIds = oldUsers.map(user => user.id);
+        const newUsersIds = [...new Set(this.state.messages.map(message => message.userId))];
+        const toLoad = newUsersIds.filter(id => !oldUsersIds.includes(id));
+
+        if (!toLoad.length) return;
+
+        return Promise.all(toLoad.map(id => apiService.user.getById(id)))
+            .then(responses => responses.map(response => response.data))
+            .then(newUsers => this.setState({ users: [...oldUsers, ...newUsers] }));
     }
 
     render() {
-        const {serverMessages} = this.state;
-
-        return <>
-            <h1>Chat</h1>
-            <Form sendMessage={(newMessage) => this.sendMessage(newMessage)}/>
-            <MessagesList messages={serverMessages}/>
-        </>
+        const { messages } = this.state;
+        return (
+            <>
+                <h1>Чат</h1>
+                <MessageForm postMessage={data => this.postMessage(data)} />
+                <MessagesList messages={messages} />
+            </>
+        );
     }
 }
 
