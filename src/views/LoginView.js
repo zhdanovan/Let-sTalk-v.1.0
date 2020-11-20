16de1174d227f675660d87343f64a8acac40c141
Artem <artemcka.jdanov@yandex.ru>
2020-11-21 00:06:45 +0700

    Update LoginView.js

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,71 +1,65 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import apiServices from "../apiServices";
+import apiService from '../apiService';
 
 export default class LoginView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            nickname: "",
-            password: "",
-            successMessage: "",
-            errorMessage: ""
+            nickname: '',
+            password: '',
+            result: null,
+            error: null
         };
     }
 
     handleSubmit(e) {
-        const {nickname, password} = this.state;
-        e.preventDefault();
         this.setState({
-            successMessage: "",
-            errorMessage: ""
+            result: null,
+            error: null
         });
-        apiServices.auth
-            .login({nickname, password})
-            .then(() => this.setState({successMessage: "User registered successfully"}))
-            .then(() => setTimeout(() => this.props.history.push('/profile'), 2000))
-            .catch(error => this.setState({errorMessage: "Error! " + error.response.data.error}));
+        apiService.auth
+            .login({
+                nickname: this.state.nickname,
+                password: this.state.password
+            })
+            .then(() => {
+                this.setState({ result: 'Успешно!' });
+                setTimeout(() => this.props.history.push('/profile'), 2000);
+            })
+            .catch(error => this.setState({ error: 'Ошибка' + error.response.data.error }));
+        e.preventDefault();
     }
 
     render() {
-        const {nickname, password, successMessage, errorMessage} = this.state;
         return (
             <>
-                <h1>Authentication</h1>
-                <div className="su-er">
-                    {successMessage}
-                    {errorMessage}
-                </div>
-                <form onSubmit={(e) => this.handleSubmit(e)}>
+                <h1>Логин</h1>
+                {this.state.error}
+                {this.state.result}
+                <form onSubmit={e => this.handleSubmit(e)}>
                     <div>
                         <label>
-                            Nickname:&nbsp;
+                            Никнейм:&nbsp;
                             <input
                                 type="text"
-                                className="nick-pass"
-                                value={nickname}
-                                onChange={e => this.setState({nickname: e.target.value})}
+                                value={this.state.nickname}
+                                onChange={e => this.setState({ nickname: e.target.value })}
                             />
                         </label>
                     </div>
                     <div>
                         <label>
-                            Password:&nbsp;
+                            Пароль:&nbsp;
                             <input
                                 type="password"
-                                className="nick-pass"
-                                value={password}
-                                onChange={e => this.setState({password: e.target.value})}
+                                value={this.state.password}
+                                onChange={e => this.setState({ password: e.target.value })}
                             />
                         </label>
                     </div>
-                    <button type="submit" className="button">Go!</button>
+                    <button type="submit">Войти</button>
                 </form>
             </>
         );
     }
-}
-
-LoginView.propTypes = {
-    history: PropTypes.object,
 }
