94e35eb93813f6fc28b1b11842948e0a02f45965
Artem <artemcka.jdanov@yandex.ru>
2020-11-22 00:30:26 +0700

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,9 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    '../src/**/*.stories.js' 
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-  	'@storybook/preset-create-react-app', // + для проектов CRA
-   	'@storybook/addon-actions',
-   	'@storybook/addon-docs'
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]
