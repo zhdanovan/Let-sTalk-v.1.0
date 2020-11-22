5b1c3ed16c6660868fc6516c067f8d7c20313b69
Artemius <artemcka.jdanov@yandex.ru>
2020-11-22 20:28:08 +0700

    Update ChatList.stories.js

--- src/stories/ChatList.stories.js
+++ src/stories/ChatList.stories.js
@@ -16,21 +16,21 @@ Primary.args = {
         {
             id: '1',
             createdAt: '2020-10-20T03:48:24.735Z',
-            title: 'Super chat',
+            title: ' chat',
             userId: '1',
             participants: ['1', '2']
         },
         {
             id: '2',
             createdAt: '2020-10-20T03:48:24.7351Z',
-            title: 'Another super chat',
+            title: 'Another chat',
             userId: '2',
             participants: ['1', '2']
         },
         {
             id: '3',
             createdAt: '2020-10-20T03:48:24.7352Z',
-            title: 'Another super chat 2',
+            title: 'Another  chat 2',
             userId: '2',
             participants: ['2', '3']
         }
